df3a30074bc97a97171b8f7c93fb19d1481b3409
novitrirahmadhani <novitrirahmadhani@gmail.com>
2026-03-06 19:57:40 +0700

    upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,26 +2,8 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5eJ718Kvzzo":
+      case "6dyWBJALO6L":
         Script1();
-        break;
-      case "6KBYmZrQY1G":
-        Script2();
-        break;
-      case "6c6lmgN661z":
-        Script3();
-        break;
-      case "62bWHEIOdhA":
-        Script4();
-        break;
-      case "5YMyUjDJxT1":
-        Script5();
-        break;
-      case "5rGQbrSiUUL":
-        Script6();
-        break;
-      case "6NUQ3THRzUX":
-        Script7();
         break;
   }
 }
@@ -33,41 +15,5 @@ audio.src="backsound.mp3";
 audio.load();
 audio.play();
 audio.volume=0.5
-}
-
-function Script2()
-{
-  var audio = document.getElementById('bgSongku');
-audio.volume = 0.0;
-}
-
-function Script3()
-{
-  var audio = document.getElementById('bgSongku');
-audio.volume = 0.1;
-}
-
-function Script4()
-{
-  var audio = document.getElementById('bgSongku');
-audio.volume = 0.2;
-}
-
-function Script5()
-{
-  var audio = document.getElementById('bgSongku');
-audio.volume = 0.3;
-}
-
-function Script6()
-{
-  var audio = document.getElementById('bgSongku');
-audio.volume = 0.4;
-}
-
-function Script7()
-{
-  var audio = document.getElementById('bgSongku');
-audio.volume = 0.5;
 }
 
